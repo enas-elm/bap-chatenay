7dc10a4f8542f248c1323f4371fe725a5ac05af6
Empereur <bartosikpatrick2@gmail.com>
2023-12-22 16:35:57 +0100

    🎨 style(page.tsx) : réorganiser et mettre en forme le contenu de la page d'accueil ✨ fonctionnalité(page.tsx) : ajouter un wrapper de largeur maximale pour le contenu de la page d'accueil La réorganisation et la mise en forme du contenu de la page d'accueil améliorent la lisibilité et l'esthétique de la page. L'ajout d'un wrapper de largeur maximale permet de centrer le contenu et de le rendre plus cohérent avec le reste du site.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 
 export default function Home() {
   return (
-    <main>
-    </main>
+    <MaxWidthWrapper>
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <h1 className='text-6xl font-bold'>
+          Welcome to{' '}
+          <a className='text-blue-600' href='https://nextjs.org'>
+            BAP Chatenay
+          </a>
+        </h1>
+
+        <p className='mt-3 text-2xl text-center'>
+          Log on to check whether you are suffering from professional wear and tear and get help for a potential retraining, with the help of our advisers.
+        </p>
+      </div>
+    </MaxWidthWrapper>
   )
 }
