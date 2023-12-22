08317f94f8508cd69dc60adf08f27d1396c1773a
Empereur <bartosikpatrick2@gmail.com>
2023-12-22 16:30:43 +0100

    🐛 corriger(globals.css) : mettre à jour les valeurs des variables de couleur ✨ fonctionnalité(globals.css) : ajouter une valeur personnalisée pour la variable --destructive-foreground Les valeurs des variables de couleur ont été mises à jour pour correspondre à la nouvelle palette de couleurs. De plus, une nouvelle valeur personnalisée a été ajoutée à la variable --destructive-foreground pour améliorer la lisibilité du texte sur les éléments destructifs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,6 @@ import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components
 export default function Home() {
   return (
     <main>
-      <LoginLink>Sign in</LoginLink>
-
-      <RegisterLink>Sign up</RegisterLink>
     </main>
   )
 }
