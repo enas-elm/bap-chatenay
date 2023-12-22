3cedd8d2026aa017e67d28c6ebf973cf4b37dcc7
Empereur <bartosikpatrick2@gmail.com>
2023-12-22 16:45:18 +0100

    ✨ fonctionnalité(page.tsx) : ajouter une page de tableau de bord Cette modification ajoute une nouvelle page de tableau de bord à l'application. La page de tableau de bord contient un en-tête avec un titre et une section "Mon profil" avec un titre et un paragraphe. La page est enveloppée dans un composant MaxWidthWrapper pour assurer une largeur maximale.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+
+const Dashboard = () => {
+
+    return (
+        <MaxWidthWrapper className="mt-5">
+            <h1>Commencer votre test sur l'usure professionnelle </h1>
+
+            <div className="mt-5">
+                <h2>My profile</h2>
+                <p>My profile</p>
+            </div>
+        </MaxWidthWrapper>
+    )
+}
+
+export default Dashboard
