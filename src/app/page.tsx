209a85365fd563572a4b1efa11e708d509abfec3
Empereur <bartosikpatrick2@gmail.com>
2024-01-08 15:49:03 +0100

    :sparkles: Add new ui componnet and fix nav bar to have dynamique nav bar with the user profils and the logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Navbar />
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
-          Évaluez votre <span className='text-blue-600'>Usure Professionnel</span> facilement.
+          Évaluez votre <span className='text-primary'>Usure Professionnel</span> facilement.
         </h1>
 
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,13 @@ import {
     getKindeServerSession,
 } from '@kinde-oss/kinde-auth-nextjs/server'
 import MobileNav from './MobileNav'
+import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
 
-const Navbar = () => {
-    const { getUser } = getKindeServerSession()
-    const user = getUser()
+
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
     return (
         <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
@@ -25,19 +28,44 @@ const Navbar = () => {
                     <MobileNav isAuth={!!user} />
 
                     <div className='hidden items-center space-x-4 sm:flex'>
-                        <LoginLink
-                            className={buttonVariants({
-                                variant: 'ghost',
-                                size: 'sm',
-                            })}>
-                            Sign in
-                        </LoginLink>
-                        <RegisterLink
-                            className={buttonVariants({
-                                size: 'sm',
-                            })}>
-                            Sign up
-                        </RegisterLink>
+                        {!user ? (
+                            <>
+                                <LoginLink
+                                    className={buttonVariants({
+                                        variant: 'ghost',
+                                        size: 'sm',
+                                    })}>
+                                    Sign in
+                                </LoginLink>
+                                <RegisterLink
+                                    className={buttonVariants({
+                                        size: 'sm',
+                                    })}>
+                                    Sign up
+                                </RegisterLink>
+                            </>
+                        ) : (
+                            <>
+                                <Link
+                                    href='/dashboard'
+                                    className={buttonVariants({
+                                        variant: 'ghost',
+                                        size: 'sm',
+                                    })}>
+                                    Dashboard
+                                </Link>
+
+                                <UserAccountNav
+                                    name={
+                                        !user.given_name || !user.family_name
+                                            ? 'Your Account'
+                                            : `${user.given_name} ${user.family_name}`
+                                    }
+                                    email={user.email ?? ''}
+                                    imageUrl={user.picture ?? ''}
+                                />
+                            </>
+                        )}
                     </div>
                 </div>
             </MaxWidthWrapper>
