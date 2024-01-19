21b5709916d68e5a050ce3b23525525885bfc5a8
Patrick Bartosik <patrickbartosik@MacBook-Pro-de-Patrick.local>
2024-01-19 13:44:08 +0100

    🎉 feat(columns.tsx): add columns definition for the data table in the dashboard
    🎉 feat(page.tsx): add new components and functionality to the dashboard page
    
    - Add columns definition for the data table in the dashboard
    - Add functionality to download data in the dashboard page
    - Add tabs component to switch between different sections in the dashboard page
    - Add cards to display overview statistics in the dashboard page
    - Add components for displaying recent sales and overview in the dashboard page
    - Add functionality to fetch tasks data from a JSON file in the dashboard page
    - Add data table component to display tasks in the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,214 @@
 import Navbar from "@/components/Navbar"
 import { CalendarDateRangePicker } from "./components/date-range-picker"
+import { Button } from "@ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@ui/card"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@ui/tabs"
+import { Overview } from "./components/overview"
+import { RecentSales } from "./components/recent-sales"
 
-const Dashboard = () => {
+import { promises as fs } from "fs"
+import path from "path"
+import { z } from "zod"
+
+import { columns } from "./components/columns"
+import { DataTable } from "./components/data-table"
+import { taskSchema } from "./data/schema"
+import { seeder } from "./data/seed"
+
+async function getTasks() {
+    const filePath = path.join(process.cwd(), "src/app/dashboard/data/tasks.json");
+
+    try {
+        await fs.access(filePath);
+    } catch (error) {
+        console.log("File does not exist, creating it...");
+        await seeder();
+    }
+
+    // Lire le fichier (nouvellement créé ou existant)
+    const data = await fs.readFile(filePath);
+    const tasks = JSON.parse(data.toString());
+
+    return z.array(taskSchema).parse(tasks);
+}
+
+const Dashboard = async () => {
+    const tasks = await getTasks()
 
     return (
         <>
             <Navbar />
             <div className="hidden flex-col md:flex">
                 <div className="flex-1 space-y-4 p-8 pt-6">
-                    <div className="flex items-center justify-between space-y-2">
-                        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-                        <div className="flex items-center space-x-2">
-                            <CalendarDateRangePicker />
+                    <div className="flex-1 space-y-4 p-8 pt-6">
+                        <div className="flex items-center justify-between space-y-2">
+                            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+                            <div className="flex items-center space-x-2">
+                                <CalendarDateRangePicker />
+                                <Button>Télécharger les données</Button>
+                            </div>
                         </div>
+                        <Tabs defaultValue="overview" className="space-y-4">
+                            <TabsList>
+                                <TabsTrigger value="overview">Overview</TabsTrigger>
+                                <TabsTrigger value="analytics" disabled>
+                                    Analytics
+                                </TabsTrigger>
+                                <TabsTrigger value="reports">
+                                    All Reports
+                                </TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="overview" className="space-y-4">
+                                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                                    <Card>
+                                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                            <CardTitle className="text-sm font-medium">
+                                                Nombrer totale d'utilisateurs
+                                            </CardTitle>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                className="h-4 w-4 text-muted-foreground"
+                                            >
+                                                <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                                            </svg>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className="text-2xl font-bold">45,231.89</div>
+                                            <p className="text-xs text-muted-foreground">
+                                                +20.1% depuis le mois dernier.
+                                            </p>
+                                        </CardContent>
+                                    </Card>
+                                    <Card>
+                                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                            <CardTitle className="text-sm font-medium">
+                                                Soumission de formulaire
+                                            </CardTitle>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                className="h-4 w-4 text-muted-foreground"
+                                            >
+                                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                                                <circle cx="9" cy="7" r="4" />
+                                                <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                                            </svg>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className="text-2xl font-bold">+2350</div>
+                                            <p className="text-xs text-muted-foreground">
+                                                +180.1% depuis le mois dernier.
+                                            </p>
+                                        </CardContent>
+                                    </Card>
+                                    <Card>
+                                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                            <CardTitle className="text-sm font-medium">Réponse au formulaire</CardTitle>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                className="h-4 w-4 text-muted-foreground"
+                                            >
+                                                <rect width="20" height="14" x="2" y="5" rx="2" />
+                                                <path d="M2 10h20" />
+                                            </svg>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className="text-2xl font-bold">+12,234</div>
+                                            <p className="text-xs text-muted-foreground">
+                                                +19% depuis le mois dernier.
+                                            </p>
+                                        </CardContent>
+                                    </Card>
+                                    <Card>
+                                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                            <CardTitle className="text-sm font-medium">
+                                                Nombre d'utilisateurs actifs
+                                            </CardTitle>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                className="h-4 w-4 text-muted-foreground"
+                                            >
+                                                <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                                            </svg>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className="text-2xl font-bold">+573</div>
+                                            <p className="text-xs text-muted-foreground">
+                                                +201 depuis l'heure dernière.
+                                            </p>
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                                    <Card className="col-span-4">
+                                        <CardHeader>
+                                            <CardTitle>Overview</CardTitle>
+                                        </CardHeader>
+                                        <CardContent className="pl-2">
+                                            <Overview />
+                                        </CardContent>
+                                    </Card>
+                                    <Card className="col-span-3">
+                                        <CardHeader>
+                                            <CardTitle>Dernier Reports</CardTitle>
+                                            <CardDescription>
+                                                Vous avez 200 nouveaux rapports ce mois-ci.
+                                            </CardDescription>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <RecentSales />
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </TabsContent>
+                            <TabsContent value="reports" className="space-y-4">
+                                <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+                                    <div className="flex items-center justify-between space-y-2">
+                                        <div>
+                                            <h2 className="text-2xl font-bold tracking-tight">Voici la liste de tout les reports</h2>
+                                            <p className="text-muted-foreground">
+                                                Vous retrouverez ici la liste de tout les reports
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <DataTable data={tasks} columns={columns} />
+                                </div>
+                            </TabsContent>
+                        </Tabs>
                     </div>
                 </div>
             </div>
