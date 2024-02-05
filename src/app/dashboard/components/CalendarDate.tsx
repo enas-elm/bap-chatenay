260d14ffe4f22d0f7bcea01d55655c5ea48cd086
Patrick Bartosik <patrickbartosik@MBP-de-Patrick.lan>
2024-02-05 16:23:18 +0100

    📦 chore(route.ts): add route for creating a CSV file with all data 📦 chore(CalendarDate.tsx): add component for selecting date range and downloading data as CSV

---
+++ src/app/dashboard/components/CalendarDate.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React, { useState } from 'react'
+import { CalendarDateRangePicker } from "./date-range-picker"
+import { Button } from '@/components/ui/button'
+import { addDays } from 'date-fns';
+import { getDatePickRange } from '@/lib/apiRequest/formRequest';
+
+
+function jsonToCsv(dataObject: any) {
+    let csvString = '';
+
+    for (const [key, value] of Object.entries(dataObject)) {
+        if (!Array.isArray(value) || value.length === 0) continue;
+
+        // Add a section header for each category
+        csvString += key + '\n';
+
+        // Get headers from the first item of the array
+        const headers = Object.keys(value[0]);
+        csvString += headers.join(',') + '\n';
+
+        // Convert each item to CSV row
+        for (const item of value) {
+            const row = headers.map(header => {
+                const escaped = ('' + item[header]).replace(/"/g, '\\"');
+                return `"${escaped}"`;
+            }).join(',');
+            csvString += row + '\n';
+        }
+
+        // Add a newline to separate categories
+        csvString += '\n';
+    }
+
+    return csvString;
+}
+
+
+function CalendarDate() {
+    const [dateRange, setDateRange] = useState({ from: new Date(), to: addDays(new Date(), 7) });
+
+    const handleDownload = async () => {
+        const response = await getDatePickRange(dateRange);
+        if (response.status === 200) {
+            const csvData = jsonToCsv(response.body); // This now expects the full response body object
+            const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+            const url = URL.createObjectURL(blob);
+            const downloadLink = document.createElement('a');
+            downloadLink.href = url;
+            downloadLink.setAttribute('download', 'download.csv');
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        } else {
+            console.error('Failed to download data:', response.message);
+        }
+    };
+    
+    return (
+        <>
+            <CalendarDateRangePicker dateRange={dateRange} setDate={setDateRange} />
+            <Button onClick={handleDownload}  >Télécharger les données</Button>
+        </>
+    )
+}
+
+export default CalendarDate
